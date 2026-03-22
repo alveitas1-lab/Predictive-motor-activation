54d61d2739ea5d3fba708e5d395f0e28ca027a34
alveitas1-lab <alveitas1@gmail.com>
2026-03-21 17:49:50 -0700

    Update main.py
    I have updated the code to break position down into x,y,z coordinates measured in feet from launch position

--- main.py
+++ main.py
@@ -2,33 +2,51 @@
 
 class FlightController:
     def __init__(self):
-        self.altitude = 0
-        self.speed = 0
-        self.direction = 'N'
+        #positional coordinates
+        self.x = 0 # east/west position
+        self.y = 0 # north/south position
+        self.z = 0 # altitude
+        #velocity components
+        self.vx = 0 # speed in x
+        self.vy = 0 # speed in y
+        self.vz = 0 # speed in z
 
     def ascend(self, feet):
-        self.altitude += feet
-        print(f'Ascended to {self.altitude} feet.')
+        self.z += feet
+        print(f'Ascended to {self.z} feet.')
 
     def descend(self, feet):
-        if self.altitude - feet < 0:
-            print('Cannot descend below 0 feet!')
+        if self.z - feet < 0:
+            print('Cannot descend below z = 0 feet!')
         else:
-            self.altitude -= feet
-            print(f'Descended to {self.altitude} feet.')
+            self.z -= feet
+            print(f'Descended to {self.z} feet.')
 
-    def set_speed(self, speed):
-        self.speed = speed
-        print(f'Speed set to {self.speed} knots.')
+    def move_x(self, feet):
+        self.x += feet
+        print(f'Moved to x = {self.x} feet.')
 
-    def change_direction(self, direction):
-        self.direction = direction
-        print(f'Direction changed to {self.direction}.')
+    def move_y(self, feet):
+        self.y += feet
+        print(f'Moved to y = {self.y} feet.')
+
+    def set_velocity(self, vzx, vy, vz):
+        self.vx = vx
+        self.vy = vy
+        self.vz = vz
+        print(f'Velocity is ({self.vx}, {self.vy}, {self.vz}) ft/s')
+
+    def status(self):
+        print(f'Position: ({self.x}, {self.y}, {self.z}) feet')
+        print(f'Velocity: ({self.vx}, {self.vy}, {self.vz}) ft/s')
+    
 
 # Example Usage
 if __name__ == '__main__':
     fc = FlightController()
-    fc.ascend(1000)
-    fc.set_speed(250)
-    fc.change_direction('E')
-    fc.descend(500)
+    fc.ascend()
+    fc.move_x()
+    fc.move_y()
+    fc.set_velocity()
+    fc.descend()
+    fc.status()
